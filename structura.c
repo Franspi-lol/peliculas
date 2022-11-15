0e0a75744db84148ae3ec15dfa2618de6092f235
Franspi-lol <67322352+Franspi-lol@users.noreply.github.com>
2022-11-15 17:46:49 -0300

    Update structura.c

--- structura.c
+++ structura.c
@@ -12,11 +12,11 @@
 
 pelicula cargarPelicula()///No recibir ID por Parametro
 {
-    int id;
+//    int id;
 
 //    id=VerId(fp);
     pelicula aCargar;
-    aCargar.id=id;
+//    aCargar.id=id;
     printf("\nIngrese Titulo de la Pelicula: ");
     fflush(stdin);
     gets(aCargar.titulo);
@@ -26,9 +26,11 @@ pelicula cargarPelicula()///No recibir ID por Parametro
     printf("\nIngrese anio de la pelicula: ");
     fflush(stdin);
     scanf("%d", &aCargar.anio);
-    aCargar.genero=elegirGenero();
+//    aCargar.genero=elegirGenero();
     aCargar.estado=1;
-
+//    printf(" \n Ingrese id de la pelicula : ");
+//    scanf("%i",&aCargar.id);
+    aCargar.id=retornarIDPeliculadesdeArchivo();
     return aCargar;
 }
 
@@ -45,40 +47,88 @@ int elegirGenero()
     {
         printf("\nIngrese un genero de la siguiente lista (de 1 a 6)");
         listaGeneros();
+        fflush(stdin);
         scanf("%d", &genero);
     }
     return genero;
 }
 
-void mostrarPelicula(pelicula peli)///falta genero
+void MostrarGenero(int genero)
 {
-    printf("\nTITULO: %s", peli.titulo);
-    printf("\nAnio de la pelicula: %d", peli.anio);
-    printf("\nDirector/a: %s", peli.director);
-    if (peli.genero==1)
+    if (genero==1)
     {
         printf("\n Genero : Accion/Aventura ");
     }
-    else if (peli.genero==2)
+    else if (genero==2)
     {
         printf("\n Genero : Ciencia Ficcion ");
     }
-     else if (peli.genero==3)
+    else if (genero==3)
     {
         printf("\n Genero : Comedia ");
     }
-     else if (peli.genero==4)
+    else if (genero==4)
     {
         printf("\n Genero : Drama ");
     }
-     else if (peli.genero==5)
+    else if (genero==5)
     {
         printf("\n Genero :  Musical");
     }
-     else if (peli.genero==6)
+    else if (genero==6)
     {
         printf("\n Genero : Suspenso ");
     }
 
+}
+
+void mostrarPelicula(pelicula peli)///falta genero
+{
+    if (peli.estado!=0)
+    {
+
+        printf("\nTITULO: %s", peli.titulo);
+        printf("\nAnio de la pelicula: %d", peli.anio);
+        printf("\n ID de la Pelicula : %d",peli.id);
+        printf("\nDirector/a: %s", peli.director);
+
+    }
+
+//    MostrarGenero(peli.genero);
     //printf("\n")
 }
+
+int retornarIDPeliculadesdeArchivo ()
+{
+    int id=0;
+    int size;
+    FILE* archi=fopen("IDpelicula.dat", "a+b");
+    fclose(archi);
+    archi=fopen("IDpelicula.dat", "r+b");
+    if (archi!=NULL)
+    {
+        fseek(archi,0, SEEK_END);
+        size=ftell(archi);
+        if(size==0)
+        {
+            id++;
+            fseek(archi,0,SEEK_SET);
+            fwrite(&id,sizeof(int),1,archi);
+        }
+        else
+        {
+            fseek(archi,0,SEEK_SET);
+            fread(&id, sizeof(int), 1, archi);
+            id++;
+            fseek(archi,0,SEEK_SET);
+            fwrite(&id,sizeof(int),1,archi);
+        }
+        fclose(archi);
+    }
+    else
+    {
+        printf("\nERROR ID ARCHIVO");
+    }
+    return id;
+}
+
