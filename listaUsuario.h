20a49dc0814bab6b25169bbfc58212618399c376
Franspi-lol <67322352+Franspi-lol@users.noreply.github.com>
2022-11-16 17:11:41 -0300

    Update listaUsuario.h

--- listaUsuario.h
+++ listaUsuario.h
@@ -1,10 +1,12 @@
 #ifndef LISTAUSUARIO_H_INCLUDED
 #define LISTAUSUARIO_H_INCLUDED
 #include "filapeli.h"
+#include "arbol.h"
 typedef struct
 {
     char nombre[20];
     int id;
+    int estado; ///0/1
 
 }Usuario;
 
@@ -17,6 +19,10 @@ typedef struct
 
 }nodoUsuario;
 
+nodoUsuario* iniclistaUsuario();
+nodoUsuario* crearNodoUsuario(Usuario nuevo);
 
+nodoUsuario* agregarFinal (nodoUsuario* lista, nodoUsuario* nuevonodo);
+nodoUsuario* buscarUltimo (nodoUsuario* lista);
 
 #endif // LISTAUSUARIO_H_INCLUDED
