c84bcd865c583d2bbf542cb249353dd731293488
Franspi-lol <67322352+Franspi-lol@users.noreply.github.com>
2022-11-16 17:10:58 -0300

    Update listaUsuario.c

--- listaUsuario.c
+++ listaUsuario.c
@@ -2,15 +2,49 @@
 
 nodoUsuario* iniclistaUsuario()
 {
-  return NULL;
+    return NULL;
 }
+
+
 
 nodoUsuario* crearNodoUsuario(Usuario nuevo)
 {
-  nodoUsuario* nuevonodo=(nodoUsuario*) malloc(sizeof(nodoUsuario));
-  nuevonodo->sig=NULL;
-  nuevonodo->ante=NULL;
-  nuevonodo->lista=NULL;
-  nuevonodo->usu=nuevo;
-  return nuevonodo;
+    nodoUsuario* nuevonodo=(nodoUsuario*) malloc(sizeof(nodoUsuario));
+    nuevonodo->sig=NULL;
+    nuevonodo->ante=NULL;
+
+    nuevonodo->usu=nuevo;
+    nuevonodo->lista=inicfilareproduccion();
+    return nuevonodo;
 }
+
+nodoUsuario* agregarFinal (nodoUsuario* lista, nodoUsuario* nuevonodo)
+{
+    nodoUsuario* ulti=NULL;
+    if(lista==NULL)
+    {
+        lista=nuevonodo;
+
+    }
+    else
+    {
+        ulti=buscarUltimo(lista);
+        ulti->sig=nuevonodo;
+        nuevonodo->ante=ulti;
+    }
+    return lista;
+}
+
+nodoUsuario* buscarUltimo (nodoUsuario* lista)
+{
+    nodoUsuario* rta=lista;
+    if (lista!=NULL)
+    {
+        while (rta->sig!=NULL)
+        {
+            rta=rta->sig;
+        }
+    }
+    return rta;
+}
+
