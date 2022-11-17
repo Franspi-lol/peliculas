b78b03c48a0a640395d08d11a327f9c227cc6fbf
Franspi-lol <67322352+Franspi-lol@users.noreply.github.com>
2022-11-16 21:56:40 -0300

    Update celda.c

--- celda.c
+++ celda.c
@@ -1,29 +1,46 @@
 #include "celda.h"
 
-
+int IngresarPeliculas(celda ada[],int dimension)
+{
+    pelicula peli;
+    char salir='s';
+   int validos=0;
+    while(salir=='s')
+    {
+        peli=cargarPelicula();
+        peli.genero=elegirGenero();
+        //peli.id=id++;
+        validos=alta(ada,peli.genero,peli,validos);
+        printf("\nDesea continuar S/N? ");
+        fflush(stdin);
+        scanf("%c",&salir);
+    }
+return validos;
+}
 int AgregarGenero(celda ada[], int genero,int validos)
 {
-    if(ada[validos].genero==genero)
-    {
+    ada[validos].genero=genero;
+
       ada[validos].Raiz=inicarbol();
       validos++;
-    }
+
 
     return validos;
 }
-int BuscarPosGenero (celda ada[],int genero [], int validos)
+int BuscarPosGenero (celda ada[],int genero, int validos)
 {
     int rta=-1;
     int i=0;
 
-    while ((i<validos)&&(rta==-1))
+    while ((i<validos)&&(ada[i].genero!=genero))
     {
-        if (ada[i].genero==genero)
+
+        i++;
+    }
+    if (ada[i].genero==genero)
         {
             rta=i;
         }
-        i++;
-    }
 
 return rta;
 }
@@ -31,27 +48,60 @@ return rta;
 int alta (celda ada[],int genero,pelicula peli,int validos)
 {
 
-    nodoArbol* Raiz=crearNodoArbol(peli);
+
+    nodoArbol* Arbol=crearNodoArbol(peli);
     int pos=BuscarPosGenero (ada,genero,validos);
+    printf("POS: %d", pos);
     if (pos==-1)
     {
         validos=AgregarGenero(ada,genero,validos);
         pos=validos-1;
 
     }
-    ada[pos].Raiz=Insertar(ada[pos].Raiz,peli);
+
+//    ada[pos].Raiz=Arbol;
+    //ada[pos].Raiz=inicarbol();
+    ada[pos].Raiz=InsertarNodoArbol(ada[pos].Raiz,Arbol);
     return validos;
 
 }
-
-
-
-int PasarArbol2Arreglo(celda ada [], nodoArbol* Raiz, int validos)
+void MostrarArregloPeliculas(celda ada[],int validos)
 {
+    int i=0;
+    printf(" \n TODOS LOS GENEROS");
 
+    while(i<validos)
+    {
+        printf("\n ");
+        printf("---------------------------");
+        MostrarGenero(ada[i].Raiz->dato.genero);
+        MostrarArbol(ada[i].Raiz);
+        printf("\n");
+        printf("---------------------------");
+        printf("\n ");
+        i++;
 
-
-
+    }
 
 }
+
+pelicula buscarPeliculaArreglo (celda ada[], int validos, int id)
+{
+    nodoArbol* aux=NULL;
+    nodoArbol* rta=NULL;
+    int contador=0;
+    while (contador<validos)
+    {
+        aux=BuscaPelicula(ada[contador].Raiz, id);
+        contador++;
+        if(aux->dato.id==id)
+        {
+            rta=aux;
+        }
+    }
+    return aux;
+
+}
+
+
 
