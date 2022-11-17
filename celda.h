5bafdd9e7facdfc0a7daa838c9df6870bcdd6e5e
Franspi-lol <67322352+Franspi-lol@users.noreply.github.com>
2022-11-16 21:58:05 -0300

    Update celda.h

--- celda.h
+++ celda.h
@@ -8,7 +8,12 @@ typedef struct
     nodoArbol* Raiz;
 } celda;
 
-
+int IngresarPeliculas(celda ada[],int dimension);
+int AgregarGenero(celda ada[], int genero,int validos);
+int BuscarPosGenero (celda ada[],int genero, int validos);
+int alta (celda ada[],int genero,pelicula peli,int validos);
+void MostrarArregloPeliculas(celda ada[],int validos);
+pelicula buscarPeliculaArreglo (celda ada[], int validos, int id);
 
 
 #endif // CELDA_H_INCLUDED
